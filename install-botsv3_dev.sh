aaa6c133f26eb8065f7e8cc21efe6131104e18ab
cranky-chris <84743965+cranky-chris@users.noreply.github.com>
2021-05-28 20:30:54 +0100

    moved apps to botsapps folder and updated script

--- install-botsv3_dev.sh
+++ install-botsv3_dev.sh
@@ -1,20 +1,20 @@
 #! /usr/bin/env bash
 
 # Enter your default Splunk administrative credentials below
-
-username=splunk
-password=splunksplunk
+printf "\nTo proceed with the install please provide your Splunk admin username and password\n"
+read -p "Splunk admin Username: " splunk_user
+read -sp "Splunk admin Password: " splunk_password
 splunk_home=/opt/splunk
 
 # Loop through all the Splunk Apps within the present working directory, and install them to Splunk
 
-for f in *.tgz;
-do $splunk_home/bin/splunk install app $f  -auth "$username:$password";
+for f in ./botsapps/*;
+do $splunk_home/bin/splunk install app $f  -auth "$splunk_user:$splunk_password";
 done
 
 # Install the CTF Scoreboard
 
-cd /opt/splunk/etc/apps
+cd $splunk_home/etc/apps
 git clone https://github.com/splunk/SA-ctf_scoreboard
 git clone https://github.com/splunk/SA-ctf_scoreboard_admin
 mkdir /opt/splunk/var/log/scoreboard
@@ -32,29 +32,31 @@ echo "[$(date +%H:%M:%S)]: Restarting Splunk..."
 
 # Create the Splunk Answers service account, and give it the appropriate role
 
-printf "\nCreate new Answers Service Account\n"
+printf "\nCreate a new Answers Service Account for BOTS\n"
 read -p "CTF Answers Service username: " ctf_answers_username
 read -sp "CTF Answers password: " ctf_answers_password
 
-$splunk_home/bin/splunk add user $ctf_answers_username -password $ctf_answers_password -role ctf_answers_service -auth "$username:$password"
+$splunk_home/bin/splunk add user $ctf_answers_username -password $ctf_answers_password -role ctf_answers_service -auth "$splunk_user:$splunk_password"
 
 # Echo the credentials for the Splunk Answers Service account in the Scoreboard Controller conf file
-cp /opt/splunk/etc/apps/SA-ctf_scoreboard/appserver/controllers/scoreboard_controller.config.example /opt/splunk/etc/apps/SA-ctf_scoreboard/appserver/controllers/scoreboard_controller.config
+cp $splunk_home/etc/apps/SA-ctf_scoreboard/appserver/controllers/scoreboard_controller.config.example /opt/splunk/etc/apps/SA-ctf_scoreboard/appserver/controllers/scoreboard_controller.config
 
 printf "[ScoreboardController]\nUSER = $ctf_answers_username\nPASS = $ctf_answers_password\nVKEY = Gl9uy4HXxt3Ym2z558XS\n" > $splunk_home/etc/apps/SA-ctf_scoreboard/appserver/controllers/scoreboard_controller.config
 
 # Create the CTF Admin account
-printf "\nCreate new Admin Account for CTF\n"
+printf "\nCreate new Admin Account for BOTS\n"
 read -p "CTF Admin username: " ctf_admin_username
 read -sp "CTF Admin password: " ctf_admin_password
 
-$splunk_home/bin/splunk add user $ctf_admin_username -password $ctf_admin_password -role ctf_admin -role can_delete -auth "$username:$password"
+$splunk_home/bin/splunk add user $ctf_admin_username -password $ctf_admin_password -role ctf_admin -role can_delete -auth "$splunk_user:$splunk_password"
 
 # Create the CTF Competitor Account
-printf "\nCreate competitor account\n"
+printf "\nCreate competitor account for BOTS\n"
 read -p "CTF Competitor username: " ctf_competitor_username
 read -sp "CTF Competitor password: " ctf_competitor_password
 
-$splunk_home/bin/splunk add user $ctf_competitor_username -password $ctf_competitor_password -role ctf_competitor -auth "$username:$password"
+$splunk_home/bin/splunk add user $ctf_competitor_username -password $ctf_competitor_password -role ctf_competitor -auth "$splunk_user:$splunk_password"
 
 echo "BOTSv3 Installation complete!"
+echo "New BOTS admin account: ctf_admin_username"
+echo "New competitor account: $ctf_competitor_username"
